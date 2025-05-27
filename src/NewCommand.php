7738d76e7eae385c35c2899cfc946cabcf8cc193
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-05-26 21:44:42 -0300

    feat(installer): add new command for scaffolding FilaKit applications
    Introduce a `NewCommand` class to scaffold FilaKit applications with interactive prompts and validation. Includes support for PHP version detection, Laravel version validation, and configuration of database options. Adds tests to verify functionality, compatibility with Laravel versions, and required extensions.

---
+++ src/NewCommand.php
@@ -0,0 +1,370 @@
+<?php
+
+namespace Filakit\Installer\Console;
+
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Composer;
+use Illuminate\Support\ProcessUtils;
+use Illuminate\Support\Str;
+use RuntimeException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\PhpExecutableFinder;
+use Symfony\Component\Process\Process;
+use function Laravel\Prompts\text;
+
+class NewCommand extends Command
+{
+    use Concerns\ConfiguresPrompts;
+    use Concerns\InteractsWithHerdOrValet;
+
+    /**
+     * The Composer instance.
+     *
+     * @var \Illuminate\Support\Composer
+     */
+    protected $composer;
+
+    /**
+     * Determine if the application is using Laravel 11 or newer.
+     *
+     * @param int $usingVersion
+     * @param string $directory
+     * @return bool
+     */
+    public function usingLaravelVersionOrNewer(int $usingVersion, string $directory): bool
+    {
+        $version = json_decode(file_get_contents($directory . '/composer.json'), true)['require']['laravel/framework'];
+        $version = str_replace('^', '', $version);
+        $version = explode('.', $version)[0];
+
+        return $version >= $usingVersion;
+    }
+
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this
+            ->setName('new')
+            ->setDescription('Create a new FilaKit application')
+            ->addArgument('name', InputArgument::REQUIRED)
+            ->addOption('database', null, InputOption::VALUE_REQUIRED, 'The database driver your application will use')
+            ->addOption('force', 'f', InputOption::VALUE_NONE, 'Forces install even if the directory already exists');
+    }
+
+    /**
+     * Interact with the user before validating the input.
+     *
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @return void
+     */
+    protected function interact(InputInterface $input, OutputInterface $output): void
+    {
+        parent::interact($input, $output);
+
+        $this->configurePrompts($input, $output);
+
+        $output->write(PHP_EOL . '  <fg=red> _                               _
+  ______ _ _       _    _ _   
+  |  ____(_) |     | |  (_) |  
+  | |__   _| | __ _| | ___| |_ 
+  |  __| | | |/ _` | |/ / | __|
+  | |    | | | (_| |   <| | |_ 
+  |_|    |_|_|\__,_|_|\_\_|\__|</>' . PHP_EOL . PHP_EOL);
+
+        $this->ensureExtensionsAreAvailable($input, $output);
+
+        if (!$input->getArgument('name')) {
+            $input->setArgument('name', text(
+                label: 'What is the name of your project?',
+                placeholder: 'E.g. example-app',
+                required: 'The project name is required.',
+                validate: function ($value) use ($input) {
+                    if (preg_match('/[^\pL\pN\-_.]/', $value) !== 0) {
+                        return 'The name may only contain letters, numbers, dashes, underscores, and periods.';
+                    }
+
+                    if ($input->getOption('force') !== true) {
+                        try {
+                            $this->verifyApplicationDoesntExist($this->getInstallationDirectory($value));
+                        } catch (RuntimeException $e) {
+                            return 'Application already exists.';
+                        }
+                    }
+                },
+            ));
+        }
+
+        if ($input->getOption('force') !== true) {
+            $this->verifyApplicationDoesntExist(
+                $this->getInstallationDirectory($input->getArgument('name'))
+            );
+        }
+    }
+
+    /**
+     * Ensure that the required PHP extensions are installed.
+     *
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @return void
+     *
+     * @throws \RuntimeException
+     */
+    private function ensureExtensionsAreAvailable(InputInterface $input, OutputInterface $output): void
+    {
+        $availableExtensions = get_loaded_extensions();
+
+        $missingExtensions = collect([
+            'ctype',
+            'filter',
+            'hash',
+            'mbstring',
+            'openssl',
+            'session',
+            'tokenizer',
+        ])->reject(fn($extension) => in_array($extension, $availableExtensions));
+
+        if ($missingExtensions->isEmpty()) {
+            return;
+        }
+
+        throw new \RuntimeException(
+            sprintf('The following PHP extensions are required but are not installed: %s', $missingExtensions->join(', ', ', and '))
+        );
+    }
+
+    /**
+     * Verify that the application does not already exist.
+     *
+     * @param string $directory
+     * @return void
+     */
+    protected function verifyApplicationDoesntExist(string $directory): void
+    {
+        if ((is_dir($directory) || is_file($directory)) && $directory != getcwd()) {
+            throw new RuntimeException('Application already exists!');
+        }
+    }
+
+    /**
+     * Get the installation directory.
+     *
+     * @param string $name
+     * @return string
+     */
+    protected function getInstallationDirectory(string $name): string
+    {
+        return $name !== '.' ? getcwd() . '/' . $name : '.';
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->validateDatabaseOption($input);
+
+        $name = rtrim($input->getArgument('name'), '/\\');
+
+        $directory = $this->getInstallationDirectory($name);
+
+        $this->composer = new Composer(new Filesystem(), $directory);
+
+        if (!$input->getOption('force')) {
+            $this->verifyApplicationDoesntExist($directory);
+        }
+
+        if ($input->getOption('force') && $directory === '.') {
+            throw new RuntimeException('Cannot use --force option when using current directory for installation!');
+        }
+
+        $composer = $this->findComposer();
+        $phpBinary = $this->phpBinary();
+
+        $createProjectCommand = $composer . " create-project jeffersongoncalves/filakit \"$directory\" --remove-vcs --prefer-dist --no-scripts";
+
+        $commands = [
+            $createProjectCommand,
+            $composer . " run post-root-package-install -d \"$directory\"",
+            $phpBinary . " \"$directory/artisan\" key:generate --ansi",
+        ];
+
+        if ($directory != '.' && $input->getOption('force')) {
+            if (PHP_OS_FAMILY == 'Windows') {
+                array_unshift($commands, "(if exist \"$directory\" rd /s /q \"$directory\")");
+            } else {
+                array_unshift($commands, "rm -rf \"$directory\"");
+            }
+        }
+
+        if (PHP_OS_FAMILY != 'Windows') {
+            $commands[] = "chmod 755 \"$directory/artisan\"";
+        }
+        if (($process = $this->runCommands($commands, $input, $output))->isSuccessful()) {
+            if ($name !== '.') {
+                $this->replaceInFile(
+                    'APP_URL=http://localhost',
+                    'APP_URL=' . $this->generateAppUrl($name, $directory),
+                    $directory . '/.env'
+                );
+            }
+        }
+
+        return $process->getExitCode();
+    }
+
+    /**
+     * Validate the database driver input.
+     *
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     */
+    protected function validateDatabaseOption(InputInterface $input): void
+    {
+        if ($input->getOption('database') && !in_array($input->getOption('database'), $drivers = ['mysql', 'mariadb', 'pgsql', 'sqlite', 'sqlsrv'])) {
+            throw new \InvalidArgumentException("Invalid database driver [{$input->getOption('database')}]. Valid options are: " . implode(', ', $drivers) . '.');
+        }
+    }
+
+    /**
+     * Get the composer command for the environment.
+     *
+     * @return string
+     */
+    protected function findComposer(): string
+    {
+        return implode(' ', $this->composer->findComposer());
+    }
+
+    /**
+     * Get the path to the appropriate PHP binary.
+     *
+     * @return string
+     */
+    protected function phpBinary(): string
+    {
+        $phpBinary = function_exists('Illuminate\Support\php_binary')
+            ? \Illuminate\Support\php_binary()
+            : (new PhpExecutableFinder)->find(false);
+
+        return $phpBinary !== false
+            ? ProcessUtils::escapeArgument($phpBinary)
+            : 'php';
+    }
+
+    /**
+     * Run the given commands.
+     *
+     * @param array $commands
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @param string|null $workingPath
+     * @param array $env
+     * @return \Symfony\Component\Process\Process
+     */
+    protected function runCommands(array $commands, InputInterface $input, OutputInterface $output, ?string $workingPath = null, array $env = []): Process
+    {
+        if (!$output->isDecorated()) {
+            $commands = array_map(function ($value) {
+                if (Str::startsWith($value, ['chmod', 'git', $this->phpBinary() . ' ./vendor/bin/pest'])) {
+                    return $value;
+                }
+
+                return $value . ' --no-ansi';
+            }, $commands);
+        }
+
+        if ($input->getOption('quiet')) {
+            $commands = array_map(function ($value) {
+                if (Str::startsWith($value, ['chmod', 'git', $this->phpBinary() . ' ./vendor/bin/pest'])) {
+                    return $value;
+                }
+
+                return $value . ' --quiet';
+            }, $commands);
+        }
+
+        $process = Process::fromShellCommandline(implode(' && ', $commands), $workingPath, $env, null, null);
+
+        if ('\\' !== DIRECTORY_SEPARATOR && file_exists('/dev/tty') && is_readable('/dev/tty')) {
+            try {
+                $process->setTty(true);
+            } catch (RuntimeException $e) {
+                $output->writeln('  <bg=yellow;fg=black> WARN </> ' . $e->getMessage() . PHP_EOL);
+            }
+        }
+
+        $process->run(function ($type, $line) use ($output) {
+            $output->write('    ' . $line);
+        });
+
+        return $process;
+    }
+
+    /**
+     * Replace the given string in the given file.
+     *
+     * @param string|array $search
+     * @param string|array $replace
+     * @param string $file
+     * @return void
+     */
+    protected function replaceInFile(string|array $search, string|array $replace, string $file): void
+    {
+        file_put_contents(
+            $file,
+            str_replace($search, $replace, file_get_contents($file))
+        );
+    }
+
+    /**
+     * Generate a valid APP_URL for the given application name.
+     *
+     * @param string $name
+     * @param string $directory
+     * @return string
+     */
+    protected function generateAppUrl(string $name, string $directory): string
+    {
+        if (!$this->isParkedOnHerdOrValet($directory)) {
+            return 'http://localhost:8000';
+        }
+
+        $hostname = mb_strtolower($name) . '.' . $this->getTld();
+
+        return $this->canResolveHostname($hostname) ? 'http://' . $hostname : 'http://localhost';
+    }
+
+    /**
+     * Get the TLD for the application.
+     *
+     * @return string
+     */
+    protected function getTld(): string
+    {
+        return $this->runOnValetOrHerd('tld') ?: 'test';
+    }
+
+    /**
+     * Determine whether the given hostname is resolvable.
+     *
+     * @param string $hostname
+     * @return bool
+     */
+    protected function canResolveHostname(string $hostname): bool
+    {
+        return gethostbyname($hostname . '.') !== $hostname . '.';
+    }
+}
